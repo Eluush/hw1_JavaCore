d948bdda0f237446650a4f18167ed9859c747284
79774 <Cinnabon_09@.com>
2025-02-17 13:10:38 +0300

    hw done

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -4,12 +4,12 @@ public class Product {
     private String name;
     private int price;
 
-    public Product(String name,int price ){
-        this.name=name;
-        this.price=price;
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
